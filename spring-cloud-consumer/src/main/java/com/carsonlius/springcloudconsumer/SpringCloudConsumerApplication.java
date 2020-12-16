2fd4c6fe15fc330446a55c3cf146cfb45db6a2be
liusen <liusen@huice.com>
2020-12-16 19:11:36 +0800

    仪表盘

--- spring-cloud-consumer/src/main/java/com/carsonlius/springcloudconsumer/SpringCloudConsumerApplication.java
+++ spring-cloud-consumer/src/main/java/com/carsonlius/springcloudconsumer/SpringCloudConsumerApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableEurekaClient
 // 激活hystrix
 @EnableCircuitBreaker
+// 激活hystrix仪表盘
+@EnableHystrixDashboard
 public class SpringCloudConsumerApplication {
 
     public static void main(String[] args) {
